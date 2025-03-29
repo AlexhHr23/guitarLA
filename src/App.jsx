16e68f8ae533d32af9c97b8e22d1120c0dc7260a
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-03-29 16:49:53 -0600

    props y eventos

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,28 @@ import './App.css'
 function App() {
 
  
-  const[data, setData] = useState(db)
+  const[data, setData] = useState(db) 
+  
  
 
   return (
     <>
-      <Header/>
+      <Header/> 
 
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
-        <div className="row mt-5"> 
-            <Guitar/>
+        <div className="row mt-5">z
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+            />
+          ))}
         </div>
     </main>
-
-
+  
+ 
     <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
             <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
