5cc7004e31028628d09454275a9f98e95a3659c3
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-03-29 20:32:46 -0600

    UseMemo

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,9 +1,10 @@
+import { useMemo } from "react"
 export const Header = ({ cart }) => {
 
     //State devivado
-    const isEmpty = () => cart.length === 0
+    const isEmpty = useMemo(() => cart.length === 0, [cart])
 
-    const cartTotal = () => cart.reduce((total, item) => total + (item.quantity * item.price), 0)
+    const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart])
     
     return (
         <header className="py-5 header">
@@ -21,7 +22,7 @@ export const Header = ({ cart }) => {
                             <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
                             <div id="carrito" className="bg-white p-3">
-                                {isEmpty()  ? (
+                                {isEmpty ? (
                                     <p className="text-center">El carrito esta vacio</p>
                                 ) : (
                                     <>
@@ -75,7 +76,7 @@ export const Header = ({ cart }) => {
                                         </tbody>
                                     </table>
                              
-                                <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal()}</span></p>
+                                <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
                                 </>
                             )}
                             
